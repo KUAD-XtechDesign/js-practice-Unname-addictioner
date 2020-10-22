424716be9be7d0bdb65f00748d47a610c6e2ae61
Unname-addictioner <68629080+Unname-addictioner@users.noreply.github.com>
2020-10-22 12:40:34 +0900

    ｋ

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -6,22 +6,24 @@ $(function(){
     let num2 = 32;
 
     $("#button01").on("click",function(){
-        $("#content").append("こんにちわ！");
+        $("#content").text("こんにちわ！");
     })
 
     $("#button02").on("click",function(){
-        $("#content").append(txt1);
+        $("#content").text(txt1);
     })
 
     $("#button03").on("click",function(){
-        $("＃content").append(txt1 + txt2);
+        $("＃content").text(txt1 + txt2);
     })
 
     $("#button04").on("click",function(){
-        $("＃content").append(num1 + num2);
+        $("＃content").text(num1 + num2);
+    })
+    
+    $("#button05").on("click",function(){
+        $("＃content").text(num1 + "足す" + num2 + "は" + (num1 + num2) + "です");
     })
 
-    $("#button05").on("click",function(){
-        $("＃content").append(num1 + "足す" + num2 + "は" + (num1 + num2) + "です");
-    })
-})
+
+});
